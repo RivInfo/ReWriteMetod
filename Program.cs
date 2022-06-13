ee498f34a3b2829ac691ad0271245c134d796022
Роман Иванов <romaiv99@gmail.com>
2022-06-13 13:44:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public static class Validators
+{
+    public static int MakeValidNumber(int a, int b, int c)
+    {
+        if (a < b)
+            return b;
+        else if (a > c)
+            return c;
+        else
+            return a;
+    }
+}
